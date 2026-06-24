da980d6861b1f93e40d8debf9ef83102f36f140f
Ritika <ri.ritikaindrakumar@gmail.com>
2026-06-24 14:42:21 +0530

    version-2

--- p3.py
+++ p3.py
@@ -3,3 +3,22 @@ if str(a) == str(a)[::-1]:
    print("palindrome")
 else:
    print("not palindrome")
+
+
+
+#-------------------
+positive = []
+negetive = []
+zero = []
+ 
+all = list(range(-25,26))
+for i in all:
+   if i > 0:
+      positive.append(i)
+   elif i < 0:
+      negetive.append(i)
+   else:
+      zero.append(i)
+print("Positive:" ,positive)
+print("Negetive:",negetive)
+print("Zero:",zero)
